9bac9a1a6aa1d6d4b8fb18a3c182a8c6439d8c84
minhfli <vuvanminh.bacninh@gmail.com>
2025-05-06 21:22:17 +0700

    feat: compare enhance with gausian filter

--- src/plot_helper.py
+++ src/plot_helper.py
@@ -5,6 +5,7 @@ fig, axs = plt.subplots(3, 5)
 fig.tight_layout()
 
 fig2, axs2 = plt.subplots(1, 2, figsize=(16, 9))
+fig3, axs3 = plt.subplots(1, 2, figsize=(16, 9))
 
 
 def plot_img(ax: matplotlib.axes.Axes, img, title, cmap=None, vmin=0, vmax=255):
